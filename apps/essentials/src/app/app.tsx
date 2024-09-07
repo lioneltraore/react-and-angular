50cc180143018936cfef6568130dd6fa5a447f45
Lionel Traore <hello@lioneltraore.com>
2024-09-07 15:03:17 +0000

    [Essentials] add core concept component

--- apps/essentials/src/app/app.tsx
+++ apps/essentials/src/app/app.tsx
@@ -1,16 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+import reactImg from '../assets/react-core-concepts.png';
+import componentsImg from '../assets/components.png';
+import { Concept } from '../types/concept';
+
+const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
+
+function genRandomInit(max: number) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 export function Header() {
+  const description = reactDescriptions[genRandomInit(2)];
+
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+export function CoreConcepts(props: Concept) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -19,7 +40,19 @@ export function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcepts />
+            <CoreConcepts />
+            <CoreConcepts />
+          </ul>
+        </section>
       </main>
     </div>
   );
